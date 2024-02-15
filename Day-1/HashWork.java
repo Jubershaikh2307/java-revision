42e913da72716b3b395ca2a14098e778c08873c9
Juber Shaikh <jubershaikh2307@gmail.com>
2024-02-15 22:27:56 +0530

    Hash map

---
+++ Day-1/HashWork.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+class HashWork {
+    public static void main(String[] args) {
+        HashMap<String, String> data = new HashMap<String, String>();
+        data.put("abc", "a");
+        System.out.println(data);
+    }
+}
