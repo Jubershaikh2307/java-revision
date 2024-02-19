0425c3553421dcba35e4148992e58b92f8a50ffd
Juber Shaikh <jubershaikh2307@gmail.com>
2024-02-19 14:08:56 +0530

    second largest program complete

--- Day-2/SeconLargest.java
+++ Day-2/SeconLargest.java
@@ -2,7 +2,16 @@ import java.util.ArrayList;
 
 public class SeconLargest {
     public static void main(String[] args) {
-        int data[] = { 1, 2, 3, 4, 5 };
+        int[] data = { 1, 2, 3, 4, 5 };
+        int largest = data[0];
+        int secondLargest = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] > largest) {
+                secondLargest = largest;
+                largest = data[i];
+            }
+        }
+        System.out.println(largest + " " + secondLargest);
 
     }
 }
