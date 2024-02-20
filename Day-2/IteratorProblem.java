5f86619c8769cca7feccb9f21da3298f7bce5e84
Juber Shaikh <jubershaikh2307@gmail.com>
2024-02-20 07:52:14 +0530

    Added the git ignore and check

---
+++ Day-2/IteratorProblem.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class IteratorProblem {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        Iterator<String> it = cars.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
