9d79687e7c014ae8fe9d781df982ac9bd708cc59
Juber Shaikh <jubershaikh2307@gmail.com>
2024-02-22 14:31:49 +0530

    Link list practice

---
+++ DSA/LinkList/InsertLast.java
@@ -0,0 +1,26 @@
+package DSA.LinkList;
+
+public class InsertLast {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public static InsertLast insert(data){
+        Node new_node = new Node(d);
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        InsertLast data = new InsertLast();
+
+    }
+}
